ebaebac9e4cc61bb9cac90999108386d7e505db3
IXIN <16cosu05mo30@gmail.com>
2023-11-25 18:08:34 +0900

    やあ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import MakeSubject from './MakeSubject';
+import ExportPoints from './ExportPoints';
 
 function App() {
+  
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className='Title'>系所属点数シュミレータ</h1>
+        <MakeSubject />
+        
     </div>
+
+    </>
+
   );
 }
 
